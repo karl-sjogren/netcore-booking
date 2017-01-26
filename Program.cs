9df44b246ba8286577eb408c7e2001c721d94f72
Karl-Johan Sjogren <karl-johan.sjogren@openlibrarysolutions.com>
2017-01-26 21:05:36 +0100

    Fixed endpoint binding for heroku

--- Program.cs
+++ Program.cs
@@ -1,17 +1,22 @@
+using System;
 using System.IO;
 using Microsoft.AspNetCore.Hosting;
 
 namespace WebApplication {
     public class Program {
         public static void Main(string[] args) {
-            var host = new WebHostBuilder()
+
+
+            var builder = new WebHostBuilder()
                 .UseKestrel()
-                .UseUrls("http://*:1339")
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
-                .UseStartup<Startup>()
-                .Build();
+                .UseStartup<Startup>();
 
+            if(string.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable("ASPNETCORE_URLS")))
+                builder = builder.UseUrls("http://*:1339");
+
+            var host = builder.Build();
             host.Run();
         }
     }

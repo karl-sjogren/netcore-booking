6a509ae112b237f946ce53ee742d929ed637eaa3
Karl-Johan Sjogren <karl-johan.sjogren@openlibrarysolutions.com>
2017-01-26 19:20:13 +0100

    Updated binding

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ namespace WebApplication {
         public static void Main(string[] args) {
             var host = new WebHostBuilder()
                 .UseKestrel()
+                .UseUrls("http://*:1339")
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()

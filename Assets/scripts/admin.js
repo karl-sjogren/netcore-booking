402a425857e94d936a1d031537991c8f77a6d17b
Karl-Johan Sjogren <karl.sjogren@gmail.com>
2017-01-28 15:52:18 +0100

    Fixed hinting errors

--- Assets/scripts/admin.js
+++ Assets/scripts/admin.js
@@ -1,42 +1,6 @@
 import $ from 'jquery';
-import moment from 'moment';
 
 export default function() {
-    $(document).on('ready', () => {
-        return;
-        $.ajax({
-            type: 'GET',
-            url: '/admin/api/orders',
-            dataType: 'json',
-        }).then((result) => {
-            let table = $('#orders');
-
-            for(let i = 0; i < result.length; i++) {
-                let data = result[i];
-                
-                let paidDate = '-';
-                if(data.paid) {
-                    paidDate = moment(data.paidDate).format('YYYY-MM-DD')
-                }
-
-                let row = $(`<tr data-order-id="${data.id}">
-                                <td class="col-name">${data.name} (${data.ticketCount}st)</td>
-                                <td>${data.phone}</td>
-                                <td>${data.email}</td>
-                                <td>${moment(data.orderDate).format('YYYY-MM-DD')}</td>
-                                <td>${data.paid ? 'Japp' : 'Nepp'}</td>
-                                <td>${paidDate}</td>
-                                <td>
-                                    <button class="set-paid">Markera som betald</button>
-                                    <button class="remove">Radera</button>
-                                </td>
-                             </tr>`);
-
-                row.appendTo(table);
-            }
-        });
-    });
-
     $('body').on('click', 'button.set-paid', (e) => {
         e.preventDefault();
         let button = $(e.currentTarget);
@@ -52,7 +16,7 @@ export default function() {
             type: 'PUT',
             url: `/admin/api/orders/${orderId}/payment`,
             dataType: 'json',
-        }).then((result) => {
+        }).then((/*result*/) => {
 
         });
     });
